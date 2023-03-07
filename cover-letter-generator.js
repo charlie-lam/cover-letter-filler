d488cc2a8769b742f9d350229148a98226dbdad8
charlie lambert <char.lam@ntlworld.com>
2023-03-07 22:50:59 +0000

    File read and template literal formation working

--- cover-letter-generator.js
+++ cover-letter-generator.js
@@ -1,2 +1,35 @@
-import variables from './cover-letter-variables'
+import holder from './cover-letter-variables.js'
+import fs from 'fs';
+import path from 'path';
+
+const filePath = new URL('cover-letter.txt', import.meta.url);
+
+const generator = async () => {
+    const {companyName,
+    companyMission,
+    projectName,
+    insertValue,
+    insertSituation,
+    insertSkill,
+    insertResult,
+    insertDuty,
+    insertSkillTwo,
+    insertSituationTwo,
+    insertAction,
+    insertResultTwo,
+    insertExactLanguage,
+    insertProject,
+    insertSkillThree, 
+    insertOwnTech,
+    insertValueTwo,
+    insertField
+    } = holder
+    const text = fs.readFileSync('cover-letter-gen/cover-letter.txt','utf8')
+    const modified = eval('`' + text + '`')
+    console.log(modified)
+
+}
+
+generator();
+
 
--- cover-letter-variables.js
+++ cover-letter-variables.js
@@ -1,1 +1,22 @@
-export default
+const holder = {
+    companyName: "" ,
+    companyMission: "",
+    projectName: "",
+    insertValue: "",
+    insertSituation: "",
+    insertSkill: "",
+    insertResult: "",
+    insertDuty: "",
+    insertSkillTwo: "",
+    insertSituationTwo: "",
+    insertAction: "",
+    insertResultTwo: "",
+    insertExactLanguage: "",
+    insertProject: "",
+    insertSkillThree: "", 
+    insertOwnTech: "",
+    insertValueTwo: "",
+    insertField: ""
+}
+
+export default holder;
